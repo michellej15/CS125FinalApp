fa7a697ec97775ad250bc6a4f29eca076c960aa3
michellejun <mj15@illinois.edu>
2018-11-30 13:14:48 -0600

    Initial commit

---
+++ app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
@@ -0,0 +1,13 @@
+package com.example.michellejun.cs125finalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogOrCat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_or_cat);
+    }
+}
