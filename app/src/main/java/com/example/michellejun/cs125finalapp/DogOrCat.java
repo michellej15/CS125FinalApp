604526062f7bc8c16b56f2422666a96e30fcb0f4
michellejun <mj15@illinois.edu>
2018-11-30 13:45:01 -0600

    JSON progress

--- app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
+++ app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
@@ -2,6 +2,12 @@ package com.example.michellejun.cs125finalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DogOrCat extends AppCompatActivity {
 
@@ -12,3 +18,26 @@ public class DogOrCat extends AppCompatActivity {
     }
 
 }
+    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            Request.Method.GET,
+            "",
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(final JSONObject response) {
+                    Log.d(TAG, response.toString());
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(final VolleyError error) {
+            Log.w(TAG, error.toString());
+        }
+    }) {
+        @Override
+        public Map<String, String> getHeaders() {
+            Map<String, String>  params = new HashMap<String, String>();
+            params.put("Ocp-Apim-Subscription-Key", "YOUR_API_KEY");
+            Log.d(TAG, params.toString());
+            return params;
+        }
+    };
