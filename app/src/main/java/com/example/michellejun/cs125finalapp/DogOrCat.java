45a387b737318dfabfb5ea2293e88cf6fb06273b
michellejun <mj15@illinois.edu>
2018-12-01 12:27:24 -0600

    Button methods, endGame screen, and got all the web APIs

--- app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
+++ app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
@@ -3,6 +3,8 @@ package com.example.michellejun.cs125finalapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONObject;
 
@@ -17,22 +19,83 @@ public class DogOrCat extends AppCompatActivity {
         setContentView(R.layout.activity_dog_or_cat);
     }
 
-}
-    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-            Request.Method.GET,
-            "",
-            null,
-            new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(final JSONObject response) {
-                    Log.d(TAG, response.toString());
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(final VolleyError error) {
-            Log.w(TAG, error.toString());
+    boolean dog = false;
+    boolean cat = false;
+    int score = 0;
+    int timer = 3;
+    boolean endGame = false;
+
+    public boolean clickDog() {
+        if (timer == 0) {
+            endGame = true;
         }
-    }) {
+        if (dog == true) {
+            score += 1;
+            timer = 3;
+        } else {
+            endGame = true;
+        }
+        return true;
+    }
+
+    public boolean clickCat() {
+        if (timer == 0) {
+            endGame = true;
+        }
+        if (cat == true) {
+            score += 1;
+            timer = 3;
+        } else {
+            endGame = true;
+        }
+        return true;
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.content_layout_id);
+
+        final Button button = findViewById(R.id.getDog);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clickDog();
+            }
+        });
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.content_layout_id);
+
+        final Button button = findViewById(R.id.getCat);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clickCat();
+            }
+        });
+    }
+}
+
+
+JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+        Request.Method.GET,
+        "",
+        null,
+        new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(final JSONObject response) {
+                Log.d(TAG, response.toString());
+                "http://dog.ceo/api/breeds/list/all";
+
+            }
+        }, new Response.ErrorListener() {}
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG, error.toString());
+            }
+        }) {
+
         @Override
         public Map<String, String> getHeaders() {
             Map<String, String>  params = new HashMap<String, String>();
@@ -41,3 +104,6 @@ public class DogOrCat extends AppCompatActivity {
             return params;
         }
     };
+
+
+
