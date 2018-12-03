8eeac27919d7bbc0befd65b9e61b1dee0f3e09e3
michellejun <mj15@illinois.edu>
2018-12-03 12:06:44 -0600

    Still want timer and parse JSON url

--- app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
+++ app/src/main/java/com/example/michellejun/cs125finalapp/DogOrCat.java
@@ -10,32 +10,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DogOrCat extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dog_or_cat);
-    }
-
     boolean dog = false;
     boolean cat = false;
-    int score = 0;
     int timer = 3;
     boolean endGame = false;
     private static final String TOTAL_COUNT = "total_count";
+    private static String TAG = "puppy";
+    private static String TAG1 = "kitty";
 
-    public void clickDog (View view) {
+    public void clickDog(View view) {
         if (dog == true) {
             TextView showCountTextView = (TextView) findViewById(R.id.getDog);
             String countString = showCountTextView.getText().toString();
@@ -60,7 +64,7 @@ public class DogOrCat extends AppCompatActivity {
     }
 
 
-    public void clickCat (View view) {
+    public void clickCat(View view) {
         TextView showCountTextView = (TextView) findViewById(R.id.getCat);
         String countString = showCountTextView.getText().toString();
         Integer count = Integer.parseInt(countString);
@@ -74,6 +78,14 @@ public class DogOrCat extends AppCompatActivity {
         } else {
             endGame = true;
         }
+        if (endGame == true) {
+            Intent winnerScore = new Intent(this, endGame.class);
+            TextView showCountText = (TextView) findViewById(R.id.get_score);
+            String countStringEnd = showCountText.getText().toString();
+            int endCount = Integer.parseInt(countStringEnd);
+            winnerScore.putExtra(TOTAL_COUNT, endCount);
+            startActivity(winnerScore);
+        }
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,27 +93,22 @@ public class DogOrCat extends AppCompatActivity {
 
         setContentView(R.layout.activity_dog_or_cat);
 
-        final Button button = findViewById(R.id.getDog);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                clickDog();
+        final Button dogButton = findViewById(R.id.getDog);
+        dogButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                clickDog(view);
+            }
+        });
+        final Button catButton = findViewById(R.id.getCat);
+        catButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                clickCat(view);
             }
         });
     }
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_dog_or_cat);
-
-        final Button button = findViewById(R.id.getCat);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                clickCat();
-            }
-        });
-    }
     public void getDogAPI() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 "http://dog.ceo/api/breeds/list/all",
                 null,
@@ -111,33 +118,59 @@ public class DogOrCat extends AppCompatActivity {
                         Log.d(TAG, response.toString());
                     }
                 }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, error.toString());
-                    }
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG, error.toString());
+            }
         });
     }
 
     public void getCatAPI() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 "https://api.thecatapi.com/v1/images/search",
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(final JSONObject response) {
-                        Log.d(TAG, response.toString());
+                        Log.d(TAG1, response.toString());
                     }
                 }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, error.toString());
-                    }
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG1, error.toString());
+            }
         });
     }
+
+    public void requestWithSomeHttpHeaders() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.thecatapi.com/v1/images/search";
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("dogPic", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR", "error => " + error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("x-api-key", "58a2cd8f-574f-4455-b912-f5ebc769600c");
+                params.put("x-api-key", "04e2b1b5-2841-4d6a-b049-57809ad2d55e");
+
+                return params;
+            }
+        };
+        queue.add(getRequest);
+    }
+
+    
 }
-
-
-
-
-
